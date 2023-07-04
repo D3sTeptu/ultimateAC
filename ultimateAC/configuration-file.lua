fea95ae8bddc8ab9f32ee040eb709d5e597ea879
Mihai <70167120+D3sTeptu@users.noreply.github.com>
2023-07-04 13:38:14 +0300

    Update configuration-file.lua

--- ultimateAC/configuration-file.lua
+++ ultimateAC/configuration-file.lua
@@ -223,23 +223,7 @@ ultimateAC = { -- Global Anti-Cheat Configuration
         },
 
         onScreenMenuDetection = {
-            active = true, -- Switch if you want [`true` or `false` ONLY!!]
-            detectableWords = { -- Detectable Words for OCR. If one of the words below is detected on player screen, he will be detected. You can also ADD/REMOVE new words.
-                "Convert Vehicle Into Ramps","injected at","Explode Players","Ram Players","Force Third Person","fallout","godmode","ANTI-CHEAT","god mode","modmenu","esx money","give armor","aimbot","trigger",
-                "triggerbot","rage bot","ragebot","rapidfire","freecam","execute","noclip","ckgangisontop","lumia1","ISMMENU","TAJNEMENUMenu","rootMenu","Outcasts666","WaveCheat","NacroxMenu","MarketMenu","topMenu",
-                "FlexSkazaMenu","SidMenu","Lynx8","LynxEvo","Maestro Menu","redEngine","HamMafia","HamHaxia","Dopameme","redMENU","Desudo","explode","gamesense","Anticheat","Tapatio","Malossi","RedStonia","Chocohax",
-                "Inyection","Inyected","Dumper","LUA Executor","Executor","Lux Menu","Event Blocker","Spectate","Wallhack","triggers","crosshair","Alokas66","Hacking System!","Destroy Menu","Server IP","Teleport To",
-                "Give Single Weapon","Airstrike Player","Taze Player","Razer Menu","Swagamine","Visual Options","d0pamine","Infinite Stamina","Blackout","Delete Vehicles Within Radius","Engine Power Boost",
-                "Flip Vehicle","Rainbow Paintjob","Combat Assiters","Damage Multiplier","Give All Weapons","Teleport To Impact","Explosive Impact","Server Nuke Options","No Ragdoll","Super Powers",
-                "Force Player Blips","Force Radar","Force Gamertags","ESX Money Options","press AV PAG","TP to Waypoint","Self Options","Vehicle options","Weapon Options","spam Vehicles","taze All",
-                "invisible all vehicles","Spam Message","Destroy Map","Give RPG","max Speed Vehicles","Rainbow All Vehicles","Delete Props","Cobra Menu","Bind Menu Key","Clone Outfit","Give Health",
-                "Teleport Into Player's Vehicle","fivesense","menu keybind","nospread","transparent props","bullet tracers","model chams","reload images","fade out in speed","cursor size","custom weapons texture",
-                "skin changer","torque multiple","override player speed","colision proof","explosion proof","copy outfit","play single particle","infinite ammo","rip server","remove ammo","remove all weapons",
-                "explosive ammo","super damage","rapid fire","Super Jump","Infinite Roll","No Criticals","Move On Water","Disable Ragdoll","CutzuSD","Vertisso","M3ny00","Pl4y_3r","W34p_On","W34p_0n","V3h1_cl3",
-                "V1s_u4l","D3str_0y","D3str_Oy","S3tt1ngs","P4rt1cl_3s","Pl4y3rz","D3l3t3","Sp4m","V3h1cl3s","T4ze","1nv1s1bll3","R41nb_0w","Sp33d","R41nb_Ow","F_ly","3xpl_0d3","Pr0pz","D3str_0y","M4p","G1v3",
-                "Rp_GG","V3h1cl3","Sl4p","D4nce","K!_ll","M3ss4ge","3mote","D4nc3","no-clip","invizibilitate","injected","Money Options","Nuke Options","Razer","Aimbot","TriggerBot","RageBot","RapidFire",
-                "fuck server","lynx","Lumia","Gamesense","Fivesense","SkidMenu","Dopamine","Explode","Teleport Options","infnite combat roll","Hydro Menu","Enter Menu Open Key"
-            },
+            active = true, -- Switch if you want [`true` or `false` ONLY!!]    
             detectionMode = 2 --> 0 = Warning ; 1 = Kick ; 2 = Ban
         }
     },
