da939a4f0998a759f9d2865efcd77da1adee5181
Mihai <70167120+D3sTeptu@users.noreply.github.com>
2023-08-15 10:31:29 +0300

    Update fxmanifest.lua

--- ultimateAC/fxmanifest.lua
+++ ultimateAC/fxmanifest.lua
@@ -5,15 +5,11 @@ lua54 'yes'
 onesync_required 'yes'
 description "The most advanced free-resource created for FiveM Server Security"
 
+shared_script "ultimateAC.lua"
 server_script "obfuscated.lua"
-
-client_scripts {
-    "loader.lua",
-    "ultimateAC.lua"
-}
+client_script "loader.lua"
 
 ui_page 'index.html'
-
 files {
 	'index.html'
 }
